e4f674cbc96081a4c29596a0b39ed9361a76d526
aluMatiSubgrA <marianmartorell.93@gmail.com>
2021-04-16 13:49:17 +0200

    yu

--- src/com/company/Metodes.java
+++ src/com/company/Metodes.java
@@ -44,6 +44,10 @@ public class Metodes {
         char[] vocals =  {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s',
                 't','u','v','w','x','y','z'};
          mystring = mystring.toLowerCase();
+         mystring = mystring.replace(".","");
+        mystring = mystring.replace(",","");
+        mystring = mystring.replace("?","");
+        mystring = mystring.replace(":","");
         String[] arrayText = mystring.split(" ");
 
         for (int i = 0; i < arrayText.length; i++) {
